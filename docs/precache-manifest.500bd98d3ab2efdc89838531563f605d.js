9b82c94fd441c1e3ce035a5983cace47abdccfb9
System Administrator <root@MacBook-Pro-Denis-2.local>
2019-02-03 03:25:07 +0500

    publish

---
+++ docs/precache-manifest.500bd98d3ab2efdc89838531563f605d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "983311365b1db12d9a38",
+    "url": "/static/js/main.98331136.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "983311365b1db12d9a38",
+    "url": "/static/css/main.cc230be1.chunk.css"
+  },
+  {
+    "revision": "4521b10970e24e3b2b059994b434c73b",
+    "url": "/index.html"
+  }
+];
